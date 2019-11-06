1ff94afda6711433488534a7fa5415e7df9f84b8
Kevin Huang <kevin.huang@bellevuecollege.edu>
2019-11-06 12:21:15 -0800

    In Class

--- for-loops/index.js
+++ for-loops/index.js
@@ -1,11 +1,21 @@
 window.onload = () => {
-    const listButton = document.getElementById('run-for-loop');
+    const elfCode = {
+        appendToList: (list, value) => {
+            const li = document.createElement("li");
+            li.appendChild(document.createTextNode(value));
+            list.appendChild(li);
+        }
+    };
 
-    listButton.onclick = () => {
+    const listButton = document.getElementById('runforLoopAction');
+
+    listButton.onclick = function() {
         const myList = document.getElementById('my-list');
             // WRAP THE NEXT LINE IN A for-loop that iterates over a variable named i. 
             // The loop should iterate 5 times (count from zero to five).
-            elfCode.appendToList(myList, i);
+            elfCode.appendToList(myList, 1);
+            elfCode.appendToList(myList, 2);
+            elfCode.appendToList(myList, 3);
             // END LOOP
     }
 }                    

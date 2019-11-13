0e2e2f94b1831aa15e4f355c53cf76d1a1d2c591
Kevin Huang <kevin.huang@bellevuecollege.edu>
2019-11-13 11:25:19 -0800

    Hi

--- javascript-tests/function-basics.js
+++ javascript-tests/function-basics.js
@@ -6,9 +6,24 @@ function functionParameters(value) {
     console.log(value);
 }
 
+function functionreturn() {
+    return "Many functions return values.";
+}
+
+function add(a, b) {
+    const addResult = a + b;
+    console.log('inside add', addResult);
+    return a + b;
+}
+
 simpleFunction();
 
 functionParamters("Many functions return values.");
+
+const addResult = add(2, 3);
+
+const returnValue = functionReturn(addResult);
+console.log(returnValue);
 
 window.onload = () => {
     const simpleFunctionDisplay = document.getElementById('simpleFunctionDisplay');

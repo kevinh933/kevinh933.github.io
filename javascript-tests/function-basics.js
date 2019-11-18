ded12f53007faa82b8d0da51159fd949c438bb0e
Kevin Huang <kevin.huang@bellevuecollege.edu>
2019-11-18 10:29:53 -0800

    Merge Problem

--- javascript-tests/function-basics.js
+++ javascript-tests/function-basics.js
@@ -28,23 +28,4 @@ console.log(returnValue);
 window.onload = () => {
     const simpleFunctionDisplay = document.getElementById('simpleFunctionDisplay');
     const functionCallsAction = document.getElementById('functionCallsAction');
-
-    function simple() {
-        console.log('simple');
-        simpleFunctionDisplay.textContent = "simple";
 }
-    }
-    functionCallsAction.onclick = simple;
-    
-    const data = nameDisplay.value;
-    
-    const elfCode = {
-        appendToList: (list, value) => {
-            const li = document.createElement("li");
-            li.appendChild(document.createTextNode(value));
-            list.appendChild(li);
-        }
-    }
-    const someAction = document.getElementById('someAction');
-    someAction.onclick = function() {
-    }

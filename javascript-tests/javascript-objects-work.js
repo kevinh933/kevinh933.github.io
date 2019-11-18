2dcbbf11bb861ef27543b5d64c69151b1019b776
kevinh933 <55712667+kevinh933@users.noreply.github.com>
2019-11-17 21:03:10 -0800

    Hello

---
+++ javascript-tests/javascript-objects-work.js
@@ -0,0 +1,12 @@
+var person = {
+    firstName: 'George',
+    lastName: 'Washington',
+    fullName: 'George Washington',
+};
+
+console.log(person);
+
+var calculator = {
+    operand01: -1,
+    operand02: -1,
+}

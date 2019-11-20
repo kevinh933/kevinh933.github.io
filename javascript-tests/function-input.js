f449d9a26ce2509bd960c08b3e456a21e29590d5
kevinh933 <55712667+kevinh933@users.noreply.github.com>
2019-11-20 13:23:17 -0800

    Quick Fix

--- javascript-tests/function-input.js
+++ javascript-tests/function-input.js
@@ -1,58 +1,54 @@
 window.onload = () => {
-    const feetToInchesAction = 
-        document.getElementById('feetToInchesAction');
+    const feetToInchesAction = document.getElementById('feetToInchesAction');
 
     feetToInchesAction.onclick = () => {
         const feetToInchesInput = document.getElementById('feetToInchesInput');
         const feetToInchesDisplay = document.getElementById('feetToInchesDisplay');
 
         feetToInchesDisplay.textContent = feetToInches(feetToInchesInput.value);
-    };
+    }
 
-const feetToInches = (feet) => {
-    return feet * 12;
-};
+    const feetToInches = (feet) => {
+        return feet * 12;
+    }
 
-    const milesToFeetAction = 
-        document.getElementById('milesToFeetAction');
+    const milesToFeetAction = document.getElementById('milesToFeetAction');
 
     milesToFeetAction.onclick = () => {
         const milesToFeetInput = document.getElementById('milesToFeetInput');
         const milesToFeetDisplay = document.getElementById('milesToFeetDisplay');
 
         milesToFeetDisplay.textContent = milesToFeet(milesToFeetInput.value);
-    };
+    }
 
-const milesToFeet = (miles) => {
-    return miles * 5280;
-};
+    const milesToFeet = (miles) => {
+        return miles * 5280;
+    }
 
-    const areaOfTriangle = 
-        document.getElementById('areaOfTriangleAction');
+    const areaOfTriangle = document.getElementById('areaOfTriangleAction');
 
     areaOfTriangleAction.onclick = () => {
-        const baseAreaOfTriangleInput = document.getElementById('baseAreaOfTriangleInput');
-        const heightAreaOfTriangleInput = document.getElementById('heightAreaOfTriangleInput');
+        const areaOfTriangleInputB = document.getElementById('areaOfTriangleInputB');
+        const areaOfTriangleInputH = document.getElementById('areaOfTriangleInputH');
         const areaOfTriangleDisplay = document.getElementById('areaOfTriangleDisplay');
 
-        areaOfTriangleDisplay.textContent = areaOfTriangle(baseAreaOfTriangleInput.value, heightAreaOfTriangleInput.value);
-    };
+        areaOfTriangleDisplay.textContent = areaOfTriangle(areaOfTriangleInputB.value, areaOfTriangleInputH.value);
+    }
 
-const areaOfTriangle = (base, height) => {
-    return base * height / 2;
-};
+    const areaOfTriangle = (base, height) => {
+        return base * height / 2;
+    }
 
-    const areaOfCircleAction = 
-        document.getElementById('areaOfCircleAction');
+    const areaOfCircleAction = document.getElementById('areaOfCircleAction');
 
     areaOfCircleAction.onclick = () => {
         const areaOfCircleInput = document.getElementById('areaOfCircleInput');
         const areaOfCircleDisplay = document.getElementById('areaOfCircleDisplay');
 
         areaOfCircleDisplay.textContent = areaOfCircle(areaOfCircleInput.value);
-    };
+    }
 
-const areaOfCircle = (radius) => {
-    return radius * radius * Math.PI;
-};
-};
+    const areaOfCircle = (radius) => {
+        return Math.pow(radius, 2) * Math.PI;
+    }
+}
